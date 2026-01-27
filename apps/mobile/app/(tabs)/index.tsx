974bddc5219aed7803b06c0530933de929966bd8
Noyon141 <mdnoyon1590852@gmail.com>
2026-01-28 00:35:17 +0600

    feat: implement authentication client and update UI components for TabOneScreen

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { Text } from "@/components/ui/text";
 import "@/global.css";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabOneScreen() {
   return (
-    <SafeAreaView className="container">
-      <Text className="text-center font-semibold text-2xl">Tab One</Text>
+    <SafeAreaView className="flex-1 bg-black justify-center items-center w-full p-4">
+      <Card className="p-6 rounded-xl border ">
+        <CardContent>
+          <Text className="text-2xl text-center font-bold  mb-2">
+            GridLock Mobile
+          </Text>
+          <Text className="text-muted-foreground text-center">
+            NativeWind is active. Backend Status: Pending connection...
+          </Text>
+        </CardContent>
+      </Card>
     </SafeAreaView>
   );
 }
